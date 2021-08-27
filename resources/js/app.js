7c4522290624fffe4243f621f93a7eea12abed20
TheDeep <theDeep1801@gmail.com>
2021-08-27 11:59:05 +0530

    initial

---
+++ resources/js/app.js
@@ -0,0 +1,120 @@
+import axios from 'axios'
+import { set } from 'mongoose'
+import Noty from  'noty'
+import { initAdmin } from './admin'
+import moment from 'moment'
+
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+
+function upadateCart(pizza) {
+    axios.post('/update-cart', pizza).then(res => {
+        cartCounter.innerText = res.data.totalQty;
+        new Noty ({
+            type: 'success',
+            timeout:1000,
+            progressBar:false,
+            text: 'Item added to cart'
+        }).show();
+    }).catch(err => {
+        new Noty({
+            type: 'error',
+            timeout: 1000,
+            progressBar: false,
+            text: 'Something went wrong!!'
+        }).show();
+    })
+}
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click', (e)=>{
+        // console.log(e);
+        let pizza = JSON.parse(btn.dataset.pizza)
+        upadateCart(pizza)
+    })
+})
+
+//Remove alert message after x sec
+const alertMsg = document.querySelector('#success-alert')
+if(alertMsg) {
+    setTimeout(() => {
+        alertMsg.remove()
+    }, 2000)
+}
+
+
+
+
+// Change order status
+let statuses = document.querySelectorAll('.status_line')
+
+let hiddenInput = document.querySelector('#hiddenInput')
+let order = hiddenInput ? hiddenInput.value : null
+order = JSON.parse(order)
+let time = document.createElement('small')
+
+
+
+
+function updateStatus(order){
+    statuses.forEach((status)=>{
+        status.classList.remove('step-completed')
+        status.classList.remove('current')
+    })
+    let stepCompleted = true;
+    statuses.forEach((status) => {
+        let dataProp = status.dataset.status
+        if(stepCompleted){
+            status.classList.add('step-completed')
+        }
+        if(dataProp === order.status){
+            stepCompleted = false
+            time.innerText = moment(order.updatedAt).format('hh:mm A')
+            status.appendChild(time)
+            if (status.nextElementSibling) {
+                status.nextElementSibling.classList.add('current')
+                
+            }
+
+        }
+    })
+
+
+}
+
+updateStatus(order)
+
+
+//Socket
+let socket = io()
+
+//join
+if (order) {
+    socket.emit('join', `order_${order._id}`)
+    
+}
+
+let adminAreaPath = window.location.pathname
+console.log(adminAreaPath);
+if (adminAreaPath.includes('admin')) {
+    initAdmin(socket)
+    socket.emit('join', 'adminRoom')
+}
+
+
+
+socket.on('orderUpdated', (data)=>{
+    const updatedOrder = {...order}
+    updatedOrder.updatedAt = moment().format()
+    updatedOrder.status = data.status
+    updateStatus(updatedOrder)
+    new Noty({
+        type: 'success',
+        timeout: 1000,
+        progressBar: false,
+        text: 'Order updated'
+    }).show();
+    
+    
+})
---
+++ webpack.mix.js
@@ -0,0 +1,33 @@
+// const { assertSupportedNodeVersion } = require('../src/Engine');
+
+// module.exports = async () => {
+//     assertSupportedNodeVersion();
+
+//     const mix = require('../src/Mix').primary;
+
+//     require(mix.paths.mix());
+
+//     await mix.installDependencies();
+//     await mix.init();
+
+//     return mix.build();
+// };
+
+
+let mix = require('laravel-mix')
+mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss', 'public/css/app.css')
+
+// const path = require('path');
+
+// module.exports = {
+//  entry: './src/index.js',
+//  entry: {
+//    index: './src/index.js',
+//    print: './src/print.js',
+//  },
+//   output: {
+//    filename: 'bundle.js',
+//    filename: '[name].bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+// };
